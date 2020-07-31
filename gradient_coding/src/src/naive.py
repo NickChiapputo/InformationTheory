c353f033ad34d77309c1e1f90b6b775955dec102
NickChiapputo <chiapputo.nick@gmail.com>
2020-07-31 15:20:22 -0500

    Added worker time breakdown and loading/vstack time breakdown.

--- gradient_coding/src/src/naive.py
+++ gradient_coding/src/src/naive.py
@@ -56,6 +56,12 @@ def naive_logistic_regression(n_procs, n_samples, n_features, input_dir, n_strag
 			print( "X_Current: {}\nBeta: {}\n".format( X_current.shape, beta.shape ) )
 			exit( 0 )
 
+		waitingTimeset = np.zeros( rounds )
+		sleepingTimeset = np.zeros( rounds )
+		computingTimeset = np.zeros( rounds )
+		sendingDataTimeset = np.zeros( rounds )
+		totalTimeset = np.zeros( rounds )
+
 	else:
 
 		msgBuffers = [ np.zeros( n_features ) for i in range( n_procs - 1 ) ]	# Initialize message buffers to send data to workers.
@@ -127,12 +133,14 @@ def naive_logistic_regression(n_procs, n_samples, n_features, input_dir, n_strag
 			regionTime = time.time()
 
 			# Randomly select worker to be straggler. Range [ 1, # of workers ]
-			straggleRank = random.SystemRandom().randint( 1, n_procs - 1 )
-			# print( "Selected rank {} to straggle.".format( straggleRank ) )
+			straggleRank = np.zeros( n_stragglers )
+			for l in range( 0, n_stragglers ):
+				straggleRank[ l ] = random.SystemRandom().randint( 1, n_procs - 1 )
+				# print( "Selected rank {} to straggle.".format( straggleRank ) )
 			for l in range(1,n_procs):
 				isStraggler[ 0 ] = 0
 
-				if l == straggleRank:
+				if l in straggleRank:
 					isStraggler[ 0 ] = 1
 
 				comm.Isend( [ beta, MPI.DOUBLE ], dest = l, tag = i )
@@ -226,10 +234,21 @@ def naive_logistic_regression(n_procs, n_samples, n_features, input_dir, n_strag
 			###
 
 		else:
+			beginTime = time.time()
+
+			####
+			startTime = beginTime
+
 			# Wait for data to be received
 			recv_reqs[ i ].Wait()
 			straggler_reqs[ i ].Wait()
 
+			waitingTimeset[ i ] = time.time() - startTime
+			####
+
+
+			####
+			startTime = time.time()
 
 			# Create a straggler by waiting a set amount of time before continuing
 			if( isStraggler ):
@@ -238,6 +257,9 @@ def naive_logistic_regression(n_procs, n_samples, n_features, input_dir, n_strag
 			# else:
 			# 	print( "Rank {}: not straggling.".format( rank ) )
 
+			sleepingTimeset[ i ] = time.time() - startTime
+			####
+
 
 			# sendTestBuf = send_req.test()
 			# if not sendTestBuf[0]:
@@ -245,87 +267,139 @@ def naive_logistic_regression(n_procs, n_samples, n_features, input_dir, n_strag
 
 
 			# Compute partial gradient
+			####
+			startTime = time.time()
+
 			predy = X_current.dot(beta)
 			g = X_current.T.dot(np.divide(y_current,np.exp(np.multiply(predy,y_current))+1))
 			g *= -1
 
+			computingTimeset[ i ] = time.time() - startTime
+			####
+
 
 			# Send data back to master
+			####
+			startTime = time.time()
+
 			send_req = comm.Isend([g, MPI.DOUBLE], dest=0, tag=i)
+			
+			sendingDataTimeset[ i ] = time.time() - startTime
+			####
+
+			totalTimeset[ i ] = time.time() - beginTime
 	#####################################################################################################
 	comm.Barrier()
 	if rank==0:
 		elapsed_time= time.time() - orig_start_time
 		print ("Total Time Elapsed: %.3f" %(elapsed_time))
 
-		# Load all training data
-		loadTime = time.time()
-		# if not is_real_data:
-		# 	X_train = load_data(input_dir+"1.dat")
-		# 	print(">> Loaded 1")
-		# 	for j in range(2,n_procs-1):
-		# 		X_temp = load_data(input_dir+str(j)+".dat")
-		# 		X_train = np.vstack((X_train, X_temp))
-		# 		print(">> Loaded "+str(j))
-		# else:
-		# 	X_train = load_sparse_csr(input_dir+"1")
-		# 	for j in range(2,n_procs-1):
-		# 		X_temp = load_sparse_csr(input_dir+str(j))
-		# 		X_train = sps.vstack((X_train, X_temp))
+		totalLoadTime = time.time()
+		loadTime = 0.0
+		vstackTime = 0.0
 
-		# y_train = load_data(input_dir+"label.dat")
-		# y_train = y_train[0:X_train.shape[0]]
+		# Load all training data		
+		if not is_real_data:
+			loadTimeStart = time.time()
 
-		# # Load all testing data
-		# y_test = load_data(input_dir + "label_test.dat")
-		# if not is_real_data:
-		# 	X_test = load_data(input_dir+"test_data.dat")
-		# else:
-		# 	X_test = load_sparse_csr(input_dir+"test_data")
+			X_train = load_data( input_dir + "1.dat" )
 
-		loadTime = time.time() - loadTime
+			loadTime += time.time() - loadTimeStart
+
+			print(">> Loaded 1")
+			for j in range( 2, n_procs - 1 ):
+				loadTimeStart = time.time()
+
+				X_temp = load_data( input_dir + str( j ) + ".dat" )
+
+				loadTime += time.time() - loadTimeStart
+
+
+				vstackTimeStart = time.time()
+
+				X_train = np.vstack( ( X_train, X_temp ) )
+
+				vstackTime += time.time() - vstackTimeStart
+
+				print( ">> Loaded " + str( j ) )
+		else:
+			loadTimeStart = time.time()
+
+			X_train = load_sparse_csr( input_dir + "1" )
+
+			loadTime += time.time() - loadTimeStart
+
+			print( ">> Loaded 1" )
+			for j in range( 2, n_procs - 1 ):
+				loadTimeStart = time.time()
+
+				X_temp = load_sparse_csr( input_dir + str( j ) )
+
+				loadTime += time.time() - loadTimeStart
+
+
+				vstackTimeStart = time.time()
+
+				X_train = np.vstack( ( X_train, X_temp ) )
+
+				vstackTime += time.time() - vstackTimeStart
+
+				print( ">> Loaded " + str( j ) )
+
+		loadTimeStart = time.time()
+
+		y_train = load_data( input_dir + "label.dat" )
+
+		loadTime += time.time() - loadTimeStart
+
+		y_train = y_train[ 0 : X_train.shape[ 0 ] ]
+
+		# Load all testing data
+		testingLoadTimeStart = time.time()
+		
+		y_test = load_data( input_dir + "label_test.dat" )
+		if not is_real_data:
+			X_test = load_data( input_dir + "test_data.dat" )
+		else:
+			X_test = load_sparse_csr( input_dir + "test_data" )
+		
+		testingLoadTime = time.time() - testingLoadTimeStart
+		totalLoadTime = time.time() - totalLoadTime
 		lossTime = time.time()
 
-		# n_train = X_train.shape[0]
-		# n_test = X_test.shape[0]
+		n_train = X_train.shape[ 0 ]
+		n_test = X_test.shape[ 0 ]
 
-		# training_loss = np.zeros(rounds)
-		# testing_loss = np.zeros(rounds)
-		# auc_loss = np.zeros(rounds)
+		training_loss = np.zeros( rounds )
+		testing_loss = np.zeros( rounds )
+		auc_loss = np.zeros( rounds )
 
-		# from sklearn.metrics import roc_curve, auc
+		from sklearn.metrics import roc_curve, auc
 
-		# for i in range(rounds):
-		# 	beta = np.squeeze(betaset[i,:])
+		for i in range( rounds ):
+			beta = np.squeeze( betaset[ i , : ] )
+			predy_train = X_train.dot( beta )
+			predy_test = X_test.dot( beta )
+			training_loss[ i ] = calculate_loss( y_train, predy_train, n_train )
+			testing_loss[ i ] = calculate_loss( y_test, predy_test, n_test )
+			fpr, tpr, thresholds = roc_curve( y_test,predy_test, pos_label = 1 )
 
-		# 	predy_train = X_train.dot(beta)
-		# 	predy_test = X_test.dot(beta)
-
-		# 	training_loss[i] = calculate_loss(y_train, predy_train, n_train)	# Calculate loss on training data
-		# 	testing_loss[i] = calculate_loss(y_test, predy_test, n_test)		# Calculate loss on testing data
-
-		# 	# Generate the ROC curve. 
-		# 	# FPR (False Positive Rate) is the x-axis, TPR (Total Positive Rate) is the y-axis
-		# 	fpr, tpr, thresholds = roc_curve(y_test,predy_test, pos_label=1)	
-
-		# 	auc_loss[i] = auc(fpr,tpr)
-
-		# 	print("Iteration %d: Train Loss = %5.3f, Test Loss = %5.3f, AUC = %5.3f, Total time taken =%5.3f"%(i, training_loss[i], testing_loss[i], auc_loss[i], timeset[i]))
-		# lossTime = time.time() - lossTime
+			auc_loss[ i ] = auc( fpr,tpr )
+			print( "Iteration %d: Train Loss = %5.3f, Test Loss = %5.3f, AUC = %5.3f, Total time taken =%5.3f" % ( i, training_loss[ i ], testing_loss[ i ], auc_loss[ i ], timeset[ i ] ) )
+		lossTime = time.time() - lossTime
 
 		saveTime = time.time()
-
-		# Get the output location. Create the directory if it does not exist.
+		
 		output_dir = input_dir + "results/"
-		if not os.path.exists(output_dir):
-			os.makedirs(output_dir)
+		if not os.path.exists( output_dir ):
+			os.makedirs( output_dir )
 
 
 		# Save simulation data.
-		# save_vector( training_loss, 	output_dir + "naive_acc_training_loss.dat" )
-		# save_vector( testing_loss, 		output_dir + "naive_acc_testing_loss.dat" )
-		# save_vector( auc_loss, 			output_dir + "naive_acc_auc.dat" )
-		save_vector( timeset,			output_dir + "naive_acc_timeset.dat" )
+		save_vector( training_loss, 	output_dir + "naive_acc_training_loss.dat" )
+		save_vector( testing_loss, 		output_dir + "naive_acc_testing_loss.dat" )
+		save_vector( auc_loss, 			output_dir + "naive_acc_auc.dat" )
+		save_vector( timeset, 			output_dir + "naive_acc_timeset.dat" )
 
 		save_vector( region1_timeset, 	output_dir + "naive_region1_timeset.dat" )
 		save_vector( region2_timeset, 	output_dir + "naive_region2_timeset.dat" )
@@ -338,7 +412,9 @@ def naive_logistic_regression(n_procs, n_samples, n_features, input_dir, n_strag
 
 		saveTime = time.time() - saveTime
 
-		print( "Time to load testing data: {}.\nTime to calculate loss: {}.\nTime to save data: {}.".format( loadTime, lossTime, saveTime ) )
+		print( "Total Load Time = %5.3f\nTesting Data Load Time: %5.3f\nTraining Data Loading Time: %5.3f (%5.3f)\nVstack Time: %5.3f (%5.3f)\nLoss Time: %5.3f\nSave Data: %5.3f\n" % ( totalLoadTime, testingLoadTime, loadTime, loadTime / ( n_procs - 1 ), vstackTime, vstackTime / ( n_procs - 2 ), lossTime, saveTime ) )
+	else:
+		print( "Rank % 2d: Total Time: % 5.3f\n         Waiting Time:      % 5.3f\n         Sleeping Time:     % 5.3f\n         Computing Time:    % 5.3f\n         Sending Data Time: % 5.3f\n" % ( rank, np.sum( totalTimeset ), np.sum( waitingTimeset ), np.sum( sleepingTimeset ), np.sum( computingTimeset ), np.sum( sendingDataTimeset ) ) )
 
 	# Synchronized ending.
 	comm.Barrier()
